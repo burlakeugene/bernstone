2d1083e04cfa229e02498c14ccccb396678c48ff
Eugene Burlak <burlakeugene@gmail.com>
2024-01-25 18:53:46 +0300

    upd

--- wp-content/themes/burlak/page-3d.php
+++ wp-content/themes/burlak/page-3d.php
@@ -2,6 +2,7 @@
   get_header();
   my_get_template_part('sections/section', [
     'header' => [
+      'breadcrumbs' => true,
       'title' => [
         'tag' => 'h1',
         'text' => get_the_title(),
