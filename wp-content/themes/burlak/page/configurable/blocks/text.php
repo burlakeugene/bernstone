812521122fd056479baf7dd85c9dd2776e4b4444
Eugene Burlak <burlakeugene@gmail.com>
2024-03-02 16:13:47 +0300

    text upgrade

--- wp-content/themes/burlak/page/configurable/blocks/text.php
+++ wp-content/themes/burlak/page/configurable/blocks/text.php
@@ -1,9 +1,15 @@
 <?php
   if($data):
+    $className = 'content';
+    foreach(['big', 'medium', 'uppercase', 'underline', 'centered'] as $key){
+      if($data['text'][$key]){
+        $className .= ' content--'.$key;
+      }
+    }
   ?>
     <?php foreach($data['text']['columns'] as $text):
       ?>
-      <div class="content <?= $data['text']['big'] ? 'content--big' : '' ?>">
+      <div class="<?= $className ?>">
         <?= $text['content'] ?>
       </div>
     <?php endforeach; ?>
